074554c0dc5a18ebfde0e45574ba0ef0074da93b
dantasg <genineto10@gmail.com>
2022-12-07 01:55:11 -0300

    limpando código

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// const { Person } = require('./person');
-
-// // require('./modules/path');
-// // require('./modules/fs');
-
-// const person = new Person('Genival');
-
-// // console.log(person.sayMyName());
-
-// require('./modules/http');
-
 const dotenv = require('dotenv');
 const connectToDatabase = require('./src/database/connect');
 
